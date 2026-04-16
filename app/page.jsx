13a0f5b59eb124b68950fe89f94ecf2ef944e387
slami96 <adam.slamen@gmail.com>
2026-04-16 15:08:09 +0200

    Add Home component with multiple child components

---
+++ app/page.jsx
@@ -0,0 +1,27 @@
+'use client';
+import { LanguageProvider } from '@/components/LanguageContext';
+import Header from '@/components/Header';
+import Hero from '@/components/Hero';
+import Story from '@/components/Story';
+import Parallax from '@/components/Parallax';
+import CraftSlider from '@/components/CraftSlider';
+import Products from '@/components/Products';
+import Trust from '@/components/Trust';
+import Contact from '@/components/Contact';
+import Footer from '@/components/Footer';
+
+export default function Home() {
+  return (
+    <LanguageProvider>
+      <Header />
+      <Hero />
+      <Story />
+      <Parallax />
+      <CraftSlider />
+      <Products />
+      <Trust />
+      <Contact />
+      <Footer />
+    </LanguageProvider>
+  );
+}
