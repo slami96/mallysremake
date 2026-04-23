a62fc0eb2b631d7127ca0a0d33e5c793d542985c
slami96 <adam.slamen@gmail.com>
2026-04-23 13:56:26 +0200

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,9 @@
 'use client';
 import Hero from '@/components/Hero';
+import Products from '@/components/Products';
 import Story from '@/components/Story';
 import Parallax from '@/components/Parallax';
 import CraftSlider from '@/components/CraftSlider';
-import Products from '@/components/Products';
 import Trust from '@/components/Trust';
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
@@ -12,10 +12,10 @@ export default function Home() {
   return (
     <main>
       <Hero />
+      <Products />
       <Story />
       <Parallax />
       <CraftSlider />
-      <Products />
       <Trust />
       <Contact />
       <Footer />
