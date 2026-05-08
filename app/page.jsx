3902254b5e792e12cadefd9469cdd92426fee502
slami96 <adam.slamen@gmail.com>
2026-05-08 21:05:43 +0200

    Refactor Home component layout
    Removed ShippingBanner and Trust components, reordered Testimonials.

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,9 @@
 'use client';
 import Hero from '@/components/Hero';
 import Products from '@/components/Products';
-import ShippingBanner from '@/components/ShippingBanner';
 import Story from '@/components/Story';
 import Testimonials from '@/components/Testimonials';
 import CraftSlider from '@/components/CraftSlider';
-import Trust from '@/components/Trust';
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
 
@@ -14,11 +12,9 @@ export default function Home() {
     <main>
       <Hero />
       <Products />
-      <ShippingBanner />
       <Story />
-      <Testimonials />
       <CraftSlider />
-      <Trust />
+      <Testimonials />
       <Contact />
       <Footer />
     </main>
